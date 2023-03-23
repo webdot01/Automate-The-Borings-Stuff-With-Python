8e5b1526eb472f3ac93c3637910feaf790b609de
Vijayalaxmi Dattatraya Wakode <vijaya@Vijayalaxmis-MacBook-Pro.local>
2023-03-24 00:12:12 +0530

    Create hierarchy

---
+++ 1. Python Basics/Day01.py
@@ -0,0 +1,10 @@
+# First program
+# https://pythontutor.com/
+print('Hello, world!')
+print("What's your name?")
+my_name = input()
+print("It's good to meet you", my_name)
+print('length of name ',len(my_name))
+print("What's your age?")
+my_age = input()
+print("Age is", my_age)
